965ba90b0f1d741e6fafa2dc909b5a9900ad9882
Ashish Deshwal <deshwalashish.deshwal5@gmail.com>
2020-05-01 00:11:38 +0530

    Added project files

---
+++ Assets/PlayClass.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayClass : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var vp = gameObject.AddComponent<UnityEngine.Video.VideoPlayer>();
+        // vp.url = "http://myserver.com/mymovie.mp4";
+
+        vp.isLooping = true;
+        vp.renderMode = UnityEngine.Video.VideoRenderMode.MaterialOverride;
+        vp.targetMaterialRenderer = GetComponent<Renderer>();
+        vp.targetMaterialProperty = "_MainTex";
+
+        vp.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
